6b7d80af885f1fa111744e1e514a8c74fcda341a
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2021-03-14 20:57:40 +0600

    ..

--- themes/mypiggyback/assets/js/main.js
+++ themes/mypiggyback/assets/js/main.js
@@ -233,8 +233,36 @@ if (windowWidth <= 767) {
       autoplay: false,
       autoplaySpeed: 4000,
       speed: 700,
-      slidesToShow: 1,
+      slidesToShow: 2,
       slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
   }
 }
