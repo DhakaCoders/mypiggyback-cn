a7830bf669399b1153a0f976dfd42a89e560c653
azizulsoftsw <azizul.softsw@gmail.com>
2021-03-17 22:29:54 +0600

    Create page-vehicle-recovery.php

---
+++ themes/mypiggyback/page-vehicle-recovery.php
@@ -0,0 +1,359 @@
+<?php 
+/*Template Name: Vehicle Recovery*/
+get_header(); 
+?>
+<section class="page-banner-sec-wrp">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="page-banner-wrp clearfix">
+          <div class="page-banner-dsc">
+            <h1 class="fl-h1">Vehicle Recovery...</h1>
+          </div>
+          <div class="breadcumbs">
+            <ul class="reset-list clearfix">
+              <li>
+                <a href="#" class="fl-home-icon">
+                  <span class="item">HOME</span>
+                </a>
+              </li>
+              <li class="active">
+                  <span>Vehicle Recovery...</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="home-page-bnr">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d66815.33056846341!2d-0.14238880307146762!3d51.51248988308131!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sbd!4v1615400813839!5m2!1sen!2sbd" width="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+  <div class="home-page-bnr-con-cntlr">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="home-page-bnr-con-inr">
+            <div class="loc-searching-form-cntlr">
+              <div style="position: relative;">
+                <form>
+                  <div class="input-field-row starting-field">
+                    <span>A</span>
+                    <input type="text" name="" placeholder="Choose your starting point">
+                  </div>
+                  <div class="input-field-row ending-field">
+                    <span>B</span>
+                    <input type="text" name="" placeholder="Choose your ending point">
+                  </div>
+                  <span class="destination-switcher-button">
+                    <i>
+                      <svg class="destination-switcher-button-icon" width="8" height="18.526" viewBox="0 0 8 18.526" fill="#ccc">
+                        <use xlink:href="#destination-switcher-button-icon"></use></svg>
+                      </i>
+                  </span>
+                </form>
+                <div class="loc-distance-miles">
+                  <div>
+                    <strong>15.6</strong>
+                    <span>miles</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="bnr-booking-form-cntlr">
+              <div class="fl-tabs bnr-booking-form-tabs">
+                <div class="vt-title tab-link current" data-tab="tab-1">
+                  <i>
+                    <svg class="info-icon" width="3.75" height="9.999" viewBox="0 0 3.75 9.999" fill="#fff">
+                    <use xlink:href="#info-icon"></use></svg>
+                  </i>
+                  <span>Vehicle Recovery Service</span>
+                </div>
+              </div>
+              <div class="vehicle-recovery-form vehicle-form">
+                <form>
+                  <div class="input-field-row">
+                    <input type="text" name="" placeholder="From location A (Postcode)">
+                  </div>
+                  <div class="input-field-row">
+                    <input type="text" name="" placeholder="To location B (Postcode)">
+                  </div>
+                  <div class="input-field-row">
+                    <input type="text" name="" placeholder="Full Name">
+                  </div>
+                  <div class="input-field-row">
+                    <input type="text" name="" placeholder="Email Address">
+                  </div>
+                  <div class="input-field-row">
+                    <input type="text" name="" placeholder="Telephone Number">
+                  </div>
+                  <div class="input-field-row input-field-row-submit">
+                    <input type="submit" name="" value="SUPER-FASTBOOKING">
+                  </div>
+                </form>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="piggyback-sec vr-piggyback-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="piggyback-cntlr">
+          <h2 class="piggyback-title left-icon-title fl-h2">
+            <span>How to book <strong>this service...</strong></span>
+            <i><svg class="title-location-icon" width="30" height="44.234" viewBox="0 0 30 44.234" fill="#ec131b">
+                  <use xlink:href="#title-location-icon"></use> </svg></i>
+          </h2>
+          <div class="piggyback-slider piggybackSlider">
+
+            <div class="piggyback-slide-item-cntlr">
+              <div class="piggyback-slide-item">
+                <div class="piggyback-item piggyback-active-item">
+                  <div class="piggyback-item-bdr">
+                    <div class="piggyback-item-img-cntlr">
+                      <img src="<?php echo THEME_URI; ?>/assets/images/piggyback-item-img-002.jpg" alt="">
+                    </div>
+                    <div class="piggyback-item-desc mHc">
+                      <p>Use your phones location to tell our<br> vehicle recovery driver where you are. Our<br> website will automatically calulate the<br> cost of recovering your vehicle.</p>
+                    </div>
+                  </div>
+                  <div class="piggyback-item-hding">
+                    <h3 class="piggyback-item-title fl-h3">1. book</h3>
+                    <span></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="piggyback-slide-item-cntlr">
+              <div class="piggyback-slide-item">
+                <div class="piggyback-item">
+                  <div class="piggyback-item-bdr">
+                    <div class="piggyback-item-img-cntlr">
+                      <img src="<?php echo THEME_URI; ?>/assets/images/piggyback-item-img-001.jpg" alt="">
+                    </div>
+                    <div class="piggyback-item-desc mHc">
+                      <p>After you have booked this service and<br> any extras, you need to tell us how<br> quickly you would like one of our<br> superheroes to your vehicles location.</p>
+                    </div>
+                  </div>
+                  <div class="piggyback-item-hding">
+                    <h3 class="piggyback-item-title fl-h3">2. wait</h3>
+                    <span></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="piggyback-slide-item-cntlr">
+              <div class="piggyback-slide-item">
+                <div class="piggyback-item">
+                  <div class="piggyback-item-bdr">
+                    <div class="piggyback-item-img-cntlr">
+                      <img src="<?php echo THEME_URI; ?>/assets/images/piggyback-item-img-003.jpg" alt="">
+                    </div>
+                    <div class="piggyback-item-desc mHc">
+                      <p>Once you've paid for your order using<br> our secure checkout it's time to relax.<br>One of our superhero piggyback team<br> has put on their cape and is on the way!</p>
+                    </div>
+                  </div>
+                  <div class="piggyback-item-hding">
+                    <h3 class="piggyback-item-title fl-h3">3. celebrate!</h3>
+                    <span></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="Vehicle-sec-wrap order-pgbk-home vr-Vehicle-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="Vehicle-sec-title fl-h2 left-icon-title show-sm">
+          <span>Our Services...</span>
+          <i><svg class="title-location-icon" width="30" height="44.234" viewBox="0 0 30 44.234" fill="#ec131b">
+            <use xlink:href="#title-location-icon"></use> </svg></i>
+          </h2>
+        </div>
+      </div>
+    </div>
+  <div>
+    <div class="Vehicle-left-bg inline-bg" style="background: url('<?php echo THEME_URI; ?>/assets/images/Vehicle-left-bg-img.jpg');">
+
+    </div>
+    <div class="Vehicle-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="Vehicle-cntlr">
+              <div class="Vehicle-desc-cntlr">
+                <div class="xs-Vehicle-left-bg inline-bg show-sm"style="background: url('<?php echo THEME_URI; ?>/assets/images/Vehicle-left-bg-img.jpg');"></div>
+                <div class="Vehicle-desc">
+                  <h2 class="Vehicle-desc-title fl-h2">Order a <br>piggyback home.</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales sagittis lorem sed mattis. Nunc rutrum mattis pulvinar. In sem velit, faucibus non mauris sed, mattis auctor purus. Etiam egestas nisl eget dapibus suscipit. Nam rutrum nisl sapien, id hendrerit nulla ullamcorper a ipsum nisl.</p>
+                  <div class="vcl-btn-cntlr">
+                    <div class="vcl-btn vcl-fst-btn">
+                      <a class="fl-red-btn" href="#">BOOK NOW</a>
+                    </div>
+                    <div class="vcl-btn vcl-scnd-btn">
+                      <a class="fl-trnsprnt-btn" href="#">OUR SERVICES</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="vcl-right-wrap">
+                <h2 class="Vehicle-title left-icon-title fl-h2 hide-sm"><span>How it works...</span>
+                  <i><svg class="title-location-icon" width="30" height="44.234" viewBox="0 0 30 44.234" fill="#ec131b">
+                    <use xlink:href="#title-location-icon"></use> </svg></i>
+                  </h2>
+                  <div class="ordr-pgbk-hm-rt-img">
+                    <img src="<?php echo THEME_URI; ?>/assets/images/order-pgbk-home-img-01.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+
+
+<section class="testimonials-sec">
+  <div class="testimonials-bg">
+    <img src="<?php echo THEME_URI; ?>/assets/images/testimonials-bg-img.png" alt="">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="testimonials-cntlr">
+          <div class="super-piggs-img">
+            <img src="<?php echo THEME_URI; ?>/assets/images/super-piggs.png" alt="">
+          </div>
+          <h2 class="testimonials-title left-icon-title fl-h2">
+            <span><strong>Super </strong>testimonials...</span>
+            <i><svg class="quadarea-icon" width="52.562" height="44" viewBox="0 0 52.562 44" fill="#f2101c">
+                  <use xlink:href="#quadarea-icon"></use> </svg></i>
+          </h2>
+          <div class="testimonials-slider-wrap">
+            <div class="testimonials-slider testimonialSlider">
+
+              <div class="tstmnl-slide-item-cntlr">
+                <div class="tstmnl-slide-item">
+                  <div class="tstmnl-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque condimentum nisl vitae ornare. Nullam dapibus, lectus vitae tristique porttitor, tortor dui congue magna, a iaculis nibh risus ut augue. Mauris ultrices est condimentum arcu tempus dictum. Vivamus fermentum, dui sit amet ultrices aliquam, magna quam gravida ipsum, vel auctor libero felis igula. </p>
+                    <div class="tstmnl-item-rating">
+                      <ul class="reset-list">
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                      </ul>
+                    </div>
+                    <h4 class="tstmnl-person-name fl-h4">Thomas Fide</h4>
+                    <h6 class="tstmnl-person-title">VEHICLE RECOVERY</h6>
+                  </div>
+                </div>
+              </div>
+
+              <div class="tstmnl-slide-item-cntlr">
+                <div class="tstmnl-slide-item">
+                  <div class="tstmnl-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque condimentum nisl vitae ornare. Nullam dapibus, lectus vitae tristique porttitor, tortor dui congue magna, a iaculis nibh risus ut augue. Mauris ultrices est condimentum arcu tempus dictum. Vivamus fermentum, dui sit amet ultrices aliquam, magna quam gravida ipsum, vel auctor libero felis igula. </p>
+                    <div class="tstmnl-item-rating">
+                      <ul class="reset-list">
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                      </ul>
+                    </div>
+                    <h4 class="tstmnl-person-name fl-h4">Thomas Fide 2</h4>
+                    <h6 class="tstmnl-person-title">VEHICLE RECOVERY</h6>
+                  </div>
+                </div>
+              </div>
+
+              <div class="tstmnl-slide-item-cntlr">
+                <div class="tstmnl-slide-item">
+                  <div class="tstmnl-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque condimentum nisl vitae ornare. Nullam dapibus, lectus vitae tristique porttitor, tortor dui congue magna, a iaculis nibh risus ut augue. Mauris ultrices est condimentum arcu tempus dictum. Vivamus fermentum, dui sit amet ultrices aliquam, magna quam gravida ipsum, vel auctor libero felis igula. </p>
+                    <div class="tstmnl-item-rating">
+                      <ul class="reset-list">
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                      </ul>
+                    </div>
+                    <h4 class="tstmnl-person-name fl-h4">Thomas Fide 3</h4>
+                    <h6 class="tstmnl-person-title">VEHICLE RECOVERY</h6>
+                  </div>
+                </div>
+              </div>
+
+              <div class="tstmnl-slide-item-cntlr">
+                <div class="tstmnl-slide-item">
+                  <div class="tstmnl-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque condimentum nisl vitae ornare. Nullam dapibus, lectus vitae tristique porttitor, tortor dui congue magna, a iaculis nibh risus ut augue. Mauris ultrices est condimentum arcu tempus dictum. Vivamus fermentum, dui sit amet ultrices aliquam, magna quam gravida ipsum, vel auctor libero felis igula. </p>
+                    <div class="tstmnl-item-rating">
+                      <ul class="reset-list">
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                      </ul>
+                    </div>
+                    <h4 class="tstmnl-person-name fl-h4">Thomas Fide 4</h4>
+                    <h6 class="tstmnl-person-title">VEHICLE RECOVERY</h6>
+                  </div>
+                </div>
+              </div>
+
+              <div class="tstmnl-slide-item-cntlr">
+                <div class="tstmnl-slide-item">
+                  <div class="tstmnl-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque condimentum nisl vitae ornare. Nullam dapibus, lectus vitae tristique porttitor, tortor dui congue magna, a iaculis nibh risus ut augue. Mauris ultrices est condimentum arcu tempus dictum. Vivamus fermentum, dui sit amet ultrices aliquam, magna quam gravida ipsum, vel auctor libero felis igula. </p>
+                    <div class="tstmnl-item-rating">
+                      <ul class="reset-list">
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                        <li><a href="#"><i class="fas fa-star"></i></a></li>
+                      </ul>
+                    </div>
+                    <h4 class="tstmnl-person-name fl-h4">Thomas Fide 5</h4>
+                    <h6 class="tstmnl-person-title">VEHICLE RECOVERY</h6>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
