9d89ba066b87c0355fae60139c1c6e496b37cac4
haque <azizul.softsw@gmail.com>
2021-05-12 17:03:07 +0600

    login

--- themes/mypiggyback/account/shortcodes/shortcode-templates/login-form.php
+++ themes/mypiggyback/account/shortcodes/shortcode-templates/login-form.php
@@ -1,24 +1,11 @@
 <?php 
   global $login_errors; 
-  var_dump($login_errors);
   $emailindex = '';
   if( isset($_POST['username']) && !empty($_POST['username']) ){
     $emailindex = $_POST['username'];
   }
 ?>
 <div class="piggy-login-form vehicle-form">
-  <form action="" method="post">
-    <div class="input-field-row">
-      <input type="text" name="username" placeholder="Username/Email Address" value="<?php echo $emailindex; ?>">
-    </div>
-    <div class="input-field-row">
-      <input type="password" name="password" placeholder="Password">
-    </div>
-    <div class="input-field-row input-field-row-submit">
-      <input type="submit" name="signin" value="Sign In">
-    </div>
-    <input type="hidden" name="user_login_nonce" value="<?php echo wp_create_nonce('user-login-nonce'); ?>"/>
-  </form>
   <?php if( array_key_exists("loging_error", $login_errors) ): ?>
   <div class="alert-msg">
     <div class="unsuccess">
@@ -26,4 +13,22 @@
     </div>
   </div>
   <?php endif; ?>
+  <form action="" method="post">
+    <div class="input-field-row">
+      <input type="text" name="username" placeholder="Username/Email Address" value="<?php echo $emailindex; ?>">
+    <?php if( array_key_exists("username", $login_errors) ): ?>
+        <?php printf('<p class="error"><span>%s</span></p>', $login_errors['username']); ?>
+    <?php endif; ?>
+    </div>
+    <div class="input-field-row">
+      <input type="password" name="password" placeholder="Password">
+      <?php if( array_key_exists("pass", $login_errors) ): ?>
+        <?php printf('<p class="error"><span>%s</span></p>', $login_errors['pass']); ?>
+      <?php endif; ?>
+    </div>
+    <div class="input-field-row input-field-row-submit">
+      <input type="submit" name="signin" value="Sign In">
+    </div>
+    <input type="hidden" name="user_login_nonce" value="<?php echo wp_create_nonce('user-login-nonce'); ?>"/>
+  </form>
 </div>
