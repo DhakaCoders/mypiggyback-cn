9648cdf8d9215f00c6e7fcb6d83bbdb98c57629a
haque <azizul.softsw@gmail.com>
2021-07-16 03:37:29 +0600

    author

--- themes/mypiggyback/account/author.php
+++ themes/mypiggyback/account/author.php
@@ -38,10 +38,10 @@ get_header();
                 </div>
               </div>
               <div class="gap-50"></div>
-              <div class="job-location1">
+              <!-- <div class="job-location1">
                 <h2 class="fl-h3">Current Location:</h2>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d460398.79861694534!2d88.56278640983301!3d25.64258531053099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fcab08431748c5%3A0x8df9a73629fb8d7b!2z4Kam4Ka_4Kao4Ka-4Kac4Kaq4KeB4KawIOCmnOCnh-CmsuCmvg!5e0!3m2!1sbn!2sbd!4v1621068290768!5m2!1sbn!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-              </div>
+              </div> -->
               <?php } ?>
               <?php }else{ ?>
                 <p><small>You have no permission to see the details.</small></p>
