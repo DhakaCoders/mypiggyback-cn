aa6095ac5e0a2dc075d87925799026c09656606f
Rannojit <cserannojitsarkar22@gmail.com>
2021-07-31 01:56:08 +0600

    .

--- themes/mypiggyback/account/shortcodes/shortcode-templates/order-payment.php
+++ themes/mypiggyback/account/shortcodes/shortcode-templates/order-payment.php
@@ -12,7 +12,30 @@
 				<div class="order-payment-sec-inr">
 					<div class="order-payment-sidebar">
 						<div class="box-white">
-							<h3 class="fl-h2">Order summary</h3>
+							<h3 class="fl-h2 ops-title">Order summary</h3>
+							<div class="order-payment-sidebar-map">
+								<div id="route_map"></div>
+							</div>
+							<div class="order-payment-sidebar-des">
+								<div class="opsd-row">
+									<div class="opsd-loc-1 opsd-loc"><strong>A:</strong> 48 Woodstock Ave, Romford RM3 9NF</div>
+								</div>
+								<div class="opsd-row">
+									<div class="opsd-loc-2 opsd-loc"><strong>B:</strong> 18 Earlsfield Dr, Chelmsford CM2 6SX</div>
+								</div>
+								<div class="opsd-row">
+									<div class="opsd-journey-time opsd-line"><span>Journey Time</span> <strong> 25 mins</strong></div>
+								</div>
+								<div class="opsd-row">
+									<div class="opsd-journey-time opsd-line"><span>Total Miles</span> <strong> 15.5  </strong></div>
+								</div>
+								<div class="opsd-row">
+									<div class="opsd-journey-time opsd-line"><span>Cost per mile</span> <strong> £2.00</strong></div>
+								</div>
+								<div class="opsd-row">
+									<div class="opsd-subtotal"><strong> Subtotal: £131</strong></div>
+								</div>
+							</div>
 						</div>
 					</div>
 					<div class="order-payment-con">
