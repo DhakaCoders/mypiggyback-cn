a6dab8458aa5f32b6a7f14bde23a6c32a278040b
Rannojit <cserannojitsarkar22@gmail.com>
2021-07-31 01:28:42 +0600

    .

--- themes/mypiggyback/account/shortcodes/shortcode-templates/order-payment.php
+++ themes/mypiggyback/account/shortcodes/shortcode-templates/order-payment.php
@@ -3,3 +3,23 @@
 		exit;
 	}
 ?>
+
+
+<section class="order-payment-sec-cntlr">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="order-payment-sec-inr">
+					<div class="order-payment-sidebar">
+						<div class="box-white">
+							<h3 class="fl-h2">Order summary</h3>
+						</div>
+					</div>
+					<div class="order-payment-con">
+						<h2 class="fl-h2 order-step-title"> <span>Step 1</span>  Your Journey</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
